9a3f6b18d57f00473d5573971f434a0d28901f13
Ash <maildoesnotexist@gmail.com>
2013-01-06 19:23:36 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Samole::Application.config.secret_token = 'cb7186993ff3e804bc685b2352879ddbd1dddb63576467fb6828579d595dc4557922200017da505b6b32b94da93b1170292f97ac5e60099ecc4c602331899c8a'
