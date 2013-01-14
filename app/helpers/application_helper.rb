53a1f8d7a097e9b9820b7a5330c6ff6e0b2586ed
Ash <maildoesnotexist@gmail.com>
2013-01-14 01:37:08 +0100

    all done with hp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+
+	def logo
+		image_tag("logo.png", :alt => "Sample App", :class => "round")
+	end
+
 	def title 
 		standart_title = "RoR Samole"
 		if @title.nil?
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -33,5 +33,19 @@ describe "LayoutLinks" do
     	response.should have_selector('title', content: "Sign Up!")
     end
 
+    it "is alternative checking " do
+    	visit root_path
+		click_link "About"
+    	response.should have_selector('title', content: "About!")
+		click_link "Help"
+    	response.should have_selector('title', content: "Help!")
+		click_link "Contact"
+    	response.should have_selector('title', content: "Contact!")
+		click_link "Home"
+    	response.should have_selector('title', content: "Home!")
+    	click_link "Signup Now!"
+    	response.should have_selector('title', content: "Sign Up!")
+    end
+
   end
 end
