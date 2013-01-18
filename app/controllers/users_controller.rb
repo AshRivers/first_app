069fb571644edec41cc5caf17cabe3d186aad26e
Ash <maildoesnotexist@gmail.com>
2013-01-18 15:36:22 +0100

    number 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   	@title = 'Sign Up!'
   end
+
+  def create 
+  	@user = User.new(params[:user])
+  	if @user.save
+      flash[:success] = "Welcome to Samole!"
+      redirect_to @user
+  	else
+  		@title = 'Sign Up!'
+  		render "new"
+  	end
+  end
+  			
 
   def show
   	@user = User.find(params[:id])
